f3222dde45a511e1e5b33bc006d9d26c9fa5bcc0
Christoph33one <cjbrowning23@hotmail.com>
2023-01-26 15:34:49 +0000

    added header title and cards with images and text of restaurant. Rough layout. Styling and css to be finished later

--- menus/views.py
+++ menus/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render
 from django.views import generic
 
 
+# Render index.html
 def index(request):
     """ Return homepage """
     return render(request, 'index.html')
 
 
+# Render restaurant.html
 def restaurant(request):
     """ Returs restaurant page """
     return render(request, 'restaurant.html')
