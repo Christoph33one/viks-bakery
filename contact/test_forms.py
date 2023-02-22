ca6db03f203155cdf31cdb8dee8078d55f0b8924
Christoph33one <cjbrowning23@hotmail.com>
2023-02-22 16:25:08 +0000

    tested contant form name and email required - tests pass

--- contact/test_forms.py
+++ contact/test_forms.py
@@ -7,5 +7,16 @@ class TestItem(TestCase):
     def test_item_name_required(self):
         form = ContactForm({'name': ''})
         self.assertFalse(form.is_valid())
-        # self.assertIn('name', form.errors.keys())
-        # self.assertEqual(form.errors, ['name'][0], 'This field is required.')
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_form_email_required(self):
+        form = ContactForm({'email': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['email'][0], 'This field is required.')
+
+    def test_form_message_not_required(self):
+        form = ContactForm({'message': ''})
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.errors['email'][0], 'This field is required.')
+
