d2bbbf15a1a05212c007520d0f8837f2ddd78eb6
Christoph33one <cjbrowning23@hotmail.com>
2023-02-23 12:06:49 +0000

    ran individualtest for menu urls. all tests pass

--- menus/test_urls.py
+++ menus/test_urls.py
@@ -1,15 +1,15 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from menus.views import CakesMenu, CreamMenu, CheeseCakeMenu
+from menus.views import index, CakesMenu, CreamMenu, CheeseCakeMenu
 
 
 class TestMenusUrls(SimpleTestCase):
 
     def Test_cake_menu_urls_reverse(self):
-        url = reverse('menus')
-        self.assertEquals(resolve(url).func, menus)
+        url = reverse('index')
+        self.assertEquals(resolve(url).func, index)
 
-    def Test_cake_menu_urls_reverse(self):
+    def Test_choc_cake_menu_urls_reverse(self):
         url = reverse('choc_cake')
         self.assertEquals(resolve(url).func.view_class, CakesMenu)
 
